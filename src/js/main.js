c2890e513538f6dfad23259133ce9389080677fc
Rayan Alamer <rayan@rayanwv.com>
2016-08-10 06:12:42 -0300

    finish items table functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,39 @@
 var cart = [];
+
+function Product(id,name, quantity, cost, total)
+{
+  this.id = id;
+  this.name = name;
+  this.quantity = quantity;
+  this.cost = cost;
+  this.total = total;
+}
+
+function addNewItem(product){
+  $('#table-content').append("<tr class=\" "+product.id+"\">\
+      <td class='text-left'>"+product.name+"</td>\
+      <td class='text-left'>"+product.cost+"</td>\
+      <td class='text-center'>\
+        <input type='number' value='1' min='1' max='1000' />\
+      </td>\
+      <td class='text-right'>"+product.total+"</td>\
+  </tr>");
+}
+
+function removeItem(product) {
+  $('.'+product.id).remove();
+}
 $(document.body).on('click', '.addToCart', function(){
+  var name = $(this).parent().parent().children('.product-item-title').text();
+  var cost = $(this).parent().children('.price').text();
   var id = this.id;
   var flaq = 1;
   for (var i = 0; i < cart.length; i++) {
-    if (cart[i] === id)
+    if (cart[i].name === name)
     {
       flaq = 0;
       cart.splice(i,1);
+      $('.'+id).remove();
       $('.cartStatus-text').fadeOut("slow");
       switch (cart.length) {
         case 0:
@@ -29,7 +56,10 @@ $(document.body).on('click', '.addToCart', function(){
 
   if (flaq)
   {
-    cart.push(id);
+    var product = new Product(id, name, '1', cost, cost);
+    cart.push(product);
+    addNewItem(product);
+    console.log(cart);
     $('.cartStatus-text').fadeOut("slow");
     switch (cart.length) {
       case 1:
